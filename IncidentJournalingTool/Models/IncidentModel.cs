f1d1eb40ce3498234c33a3cc3a801e7517606919
Steve Barr <steve.barr@tech2day.co>
2014-03-28 16:03:26 -0500

    Service added

---
+++ IncidentJournalingTool/Models/IncidentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IncidentJournalingTool.Models
+{
+    public class IncidentModel
+    {
+        public string ID { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public string CreateUser { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ IncidentJournalingTool/Services/IncidentService.cs
@@ -0,0 +1,38 @@
+﻿using IncidentJournalingTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IncidentJournalingTool.Services
+{
+    public class IncidentService
+    {
+        public IEnumerable<IncidentModel> GetByID (string ID)
+        {
+            IncidentModel mod = new IncidentModel()
+            {
+                CreateDateTime = DateTime.Now,
+                CreateUser = "Bars03",
+                ID = "1",
+                Title = "Title",
+            };
+            return new List<IncidentModel>() { mod };
+        }
+        public IEnumerable<IncidentModel> GetByDate(DateTime date)
+        {
+            IncidentModel mod = new IncidentModel()
+            {
+                CreateDateTime = DateTime.Now,
+                CreateUser = "Bars03",
+                ID = "1",
+                Title = "Title",
+            };
+            return new List<IncidentModel>() { mod };
+        }
+        public IncidentModel Create(IncidentModel model)
+        {
+            return model;
+        }
+    }
+}
